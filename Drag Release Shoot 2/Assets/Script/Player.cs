865b96e87193fbc5c40ae02083320416fb54d6b0
avodhel <uyelik-eposta@hotmail.com>
2020-02-19 21:16:47 +0300

    pretend like 2d

--- Drag Release Shoot 2/Assets/Script/Player.cs
+++ Drag Release Shoot 2/Assets/Script/Player.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Vector3 startPos;
+    private Vector3 endPos;
+    public Vector3 initPos;
+    private Rigidbody rigidbody;
+    private Vector3 forceAtPlayer;
+    public float forceFactor;
+
+    public GameObject trajectoryDot;
+    private GameObject[] trajectoryDots;
+    public int number;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        trajectoryDots = new GameObject[number];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        { //click
+            startPos = gameObject.transform.position;
+            for (int i = 0; i < number; i++)
+            {
+                trajectoryDots[i] = Instantiate(trajectoryDot, gameObject.transform);
+            }
+
+        }
+        if (Input.GetMouseButton(0))
+        { //drag
+            endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 10);
+            gameObject.transform.position = endPos;
+            forceAtPlayer = endPos - startPos;
+            for (int i = 0; i < number; i++)
+            {
+                trajectoryDots[i].transform.position = calculatePosition(i * 0.1f);
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        { //leave
+            rigidbody.useGravity = true;
+            rigidbody.velocity = new Vector2(-forceAtPlayer.x * forceFactor, -forceAtPlayer.y * forceFactor);
+            for (int i = 0; i < number; i++)
+            {
+                Destroy(trajectoryDots[i]);
+            }
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.useGravity = false;
+            rigidbody.velocity = Vector2.zero;
+            gameObject.transform.position = initPos;
+
+        }
+    }
+
+    private Vector2 calculatePosition(float elapsedTime)
+    {
+        return new Vector2(endPos.x, endPos.y) + //X0
+                new Vector2(-forceAtPlayer.x * forceFactor, 
+                            -forceAtPlayer.y * forceFactor) * elapsedTime + 0.5f * Physics2D.gravity * elapsedTime * elapsedTime;
     }
 }

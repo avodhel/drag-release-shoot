b160d9e756e8d04f3c783a1c92921390c952accc
avodhel <uyelik-eposta@hotmail.com>
2020-02-20 01:45:22 +0300

    rotate cannonball according to mouse position

--- Drag Release Shoot 2/Assets/Script/Player.cs
+++ Drag Release Shoot 2/Assets/Script/Player.cs
@@ -19,7 +19,7 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-        initPos = gameObject.transform.position;
+        initPos = Camera.main.ScreenToWorldPoint(gameObject.transform.position);
         trajectoryDots = new GameObject[number];
     }
 
@@ -30,7 +30,7 @@ public class Player : MonoBehaviour
 
     private void SpawnBall()
     {
-        GameObject instantiatedBall = Instantiate(ball, muzzlePoint.transform.position, Quaternion.identity);
+        GameObject instantiatedBall = Instantiate(ball, gameObject.transform.position, Quaternion.identity);
         rb = instantiatedBall.GetComponent<Rigidbody>();
     }
 
@@ -44,12 +44,14 @@ public class Player : MonoBehaviour
             {
                 trajectoryDots[i] = Instantiate(trajectoryDot, gameObject.transform);
             }
-
         }
+
         if (Input.GetMouseButton(0))
         { //drag
             var mousePos = Input.mousePosition;
             mousePos.z = 10; // select distance = 10 units from the camera
+
+            RotateCanonBall();
 
             endPos = Camera.main.ScreenToWorldPoint(mousePos) + new Vector3(0, 0, -10);
             forceAtPlayer = endPos - startPos;
@@ -58,6 +60,7 @@ public class Player : MonoBehaviour
                 trajectoryDots[i].transform.position = calculatePosition(i * 0.1f);
             }
         }
+
         if (Input.GetMouseButtonUp(0))
         { //leave
             rb.useGravity = true;
@@ -72,10 +75,26 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void RotateCanonBall()
+    {
+        //Get mouse position
+        Vector3 mousePos = Input.mousePosition;
+
+        //Adjust mouse z position
+        mousePos.z = Camera.main.transform.position.y - transform.position.y;
+
+        //Get a world position for the mouse
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        //Get the angle to rotate and rotate
+        float angle = -Mathf.Atan2(transform.position.z - mouseWorldPos.z, transform.position.x - mouseWorldPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, angle + 90, 0), 4 * Time.deltaTime);
+    }
+
     private Vector3 calculatePosition(float elapsedTime)
     {
         return gameObject.transform.position +
-               new Vector3(-forceAtPlayer.x * forceFactor, 
+               new Vector3(-forceAtPlayer.x * forceFactor,
                            -forceAtPlayer.y * forceFactor,
                            -forceAtPlayer.z * forceFactor) * elapsedTime + 0.5f * Physics.gravity * elapsedTime * elapsedTime;
     }

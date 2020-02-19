5feed694e58878ebd0ddb18a0f53bb894296f967
avodhel <uyelik-eposta@hotmail.com>
2020-02-19 23:45:01 +0300

    cannonball added

--- Drag Release Shoot 2/Assets/Script/Ball.cs
+++ Drag Release Shoot 2/Assets/Script/Ball.cs
@@ -2,37 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Ball : MonoBehaviour
 {
-    private Vector3 startPos;
-    private Vector3 endPos;
-    public Vector3 initPos;
-    private Rigidbody rigidbody;
-    private Vector3 forceAtPlayer;
-    public float forceFactor;
-
     public GameObject trajectoryDot;
-    private GameObject[] trajectoryDots;
+    public float forceFactor;
     public int number;
 
+    //private GameObject muzzlePoint;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private Vector3 initPos;
+    private Rigidbody rb;
+    private Vector3 forceAtPlayer;
+    private GameObject[] trajectoryDots;
 
+    //[HideInInspector]
+    //public bool drawTrajectoryControl = false;
 
-    // Start is called before the first frame update
-    void Start()
+    //private void Awake()
+    //{
+    //    muzzlePoint = GameObject.FindGameObjectWithTag("canonball").gameObject;
+    //}
+
+    private void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        initPos = gameObject.transform.position;
+        rb = GetComponent<Rigidbody>();
         trajectoryDots = new GameObject[number];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        //if (drawTrajectoryControl)
+        //{
+            UserInput();
+        //}
+    }
+
+    private void UserInput()
     {
         if (Input.GetMouseButtonDown(0))
         { //click
             startPos = gameObject.transform.position;
+            //startPos = muzzlePoint.transform.position;
             for (int i = 0; i < number; i++)
             {
                 trajectoryDots[i] = Instantiate(trajectoryDot, gameObject.transform);
+                //trajectoryDots[i] = Instantiate(trajectoryDot, muzzlePoint.transform);
             }
 
         }
@@ -42,7 +58,6 @@ public class Player : MonoBehaviour
             mousePos.z = 10; // select distance = 10 units from the camera
 
             endPos = Camera.main.ScreenToWorldPoint(mousePos) + new Vector3(0, 0, -10);
-            //gameObject.transform.position = endPos;
             forceAtPlayer = endPos - startPos;
             for (int i = 0; i < number; i++)
             {
@@ -51,9 +66,9 @@ public class Player : MonoBehaviour
         }
         if (Input.GetMouseButtonUp(0))
         { //leave
-            rigidbody.useGravity = true;
-            rigidbody.velocity = new Vector3(-forceAtPlayer.x * forceFactor, 
-                                             -forceAtPlayer.y * forceFactor, 
+            rb.useGravity = true;
+            rb.velocity = new Vector3(-forceAtPlayer.x * forceFactor,
+                                             -forceAtPlayer.y * forceFactor,
                                              -forceAtPlayer.z * forceFactor);
 
             for (int i = 0; i < number; i++)
@@ -61,18 +76,10 @@ public class Player : MonoBehaviour
                 Destroy(trajectoryDots[i]);
             }
         }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rigidbody.useGravity = false;
-            rigidbody.velocity = Vector3.zero;
-            gameObject.transform.position = initPos;
-
-        }
     }
 
     private Vector3 calculatePosition(float elapsedTime)
     {
-        //return new Vector3(endPos.x, endPos.y, endPos.z) + //X0
         return gameObject.transform.position +
                new Vector3(-forceAtPlayer.x * forceFactor, 
                            -forceAtPlayer.y * forceFactor,
---
+++ Drag Release Shoot 2/Assets/Script/CanonBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonBall : MonoBehaviour
+{
+    public Transform muzzlePoint;
+    public GameObject ball;
+
+    private void Update()
+    {
+        UserInput();
+    }
+
+    private void UserInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject instatiatedBall = Instantiate(ball, muzzlePoint.position, Quaternion.identity);
+            //instatiatedBall.GetComponent<Ball>().drawTrajectoryControl = true;
+        }
+    }
+}
